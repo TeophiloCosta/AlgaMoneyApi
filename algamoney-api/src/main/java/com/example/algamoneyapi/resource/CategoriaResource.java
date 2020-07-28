f7681c64efed73e2c988632071cb3ee55f4ada82
TeophiloCosta <66921707+TeophiloCosta@users.noreply.github.com>
2020-07-28 18:26:12 -0300

    Add files via upload

--- algamoney-api/src/main/java/com/example/algamoneyapi/resource/CategoriaResource.java
+++ algamoney-api/src/main/java/com/example/algamoneyapi/resource/CategoriaResource.java
@@ -1,6 +1,6 @@
 package com.example.algamoneyapi.resource;
 
-import java.net.URI;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,10 +16,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import org.springframework.web.bind.annotation.RestController;
+
+
+import com.example.algamoneyapi.event.RecursoCriadoEvent;
 import com.example.algamoneyapi.model.Categoria;
 import com.example.algamoneyapi.repository.CategoriaRepository;
 
@@ -30,6 +32,9 @@ public class CategoriaResource {
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 	
+	@Autowired
+	private ApplicationEventPublisher publisher;
+	
 	@GetMapping
 	public List<Categoria> listar() {
 		return categoriaRepository.findAll();
@@ -39,10 +44,9 @@ public class CategoriaResource {
 	public ResponseEntity<Categoria> criar(@Valid @RequestBody Categoria categoria, HttpServletResponse response) {
 		Categoria categoriaSalva = categoriaRepository.save(categoria);
 		
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}").buildAndExpand(categoriaSalva.getCodigo()).toUri();
-		response.setHeader("Location", uri.toASCIIString());
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, categoriaSalva.getCodigo()));
 		
-		return ResponseEntity.created(uri).body(categoriaSalva);
+		return ResponseEntity.status(HttpStatus.CREATED).body(categoriaSalva);
 	}
 	
 	@GetMapping("/{codigo}")
---
+++ algamoney-api/src/main/java/com/example/algamoneyapi/service/PessoaService.java
@@ -0,0 +1,30 @@
+package com.example.algamoneyapi.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import com.example.algamoneyapi.model.Pessoa;
+import com.example.algamoneyapi.repository.PessoaRepository;
+
+@Service
+public class PessoaService {
+	
+	@Autowired
+	private PessoaRepository pessoaRepository;
+	
+	public Pessoa atualizar(Long codigo, Pessoa pessoa) {
+		Optional<Pessoa> pessoaSalva = pessoaRepository.findById(codigo);
+				
+		if(pessoaSalva.isPresent()) {
+			BeanUtils.copyProperties(pessoa, pessoaSalva, "codigo");
+			return pessoaRepository.save(pessoaSalva.get());	
+	
+		} else
+			throw new EmptyResultDataAccessException(1);
+	}
+
+}
